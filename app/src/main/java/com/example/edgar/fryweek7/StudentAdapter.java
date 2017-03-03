d4a710290f18bbc7f3d61e402120ffa2c2438648
enrro <edgarbg585@gmail.com>
2017-03-03 17:10:53 -0600

    studnet adapter have more

--- app/src/main/java/com/example/edgar/fryweek7/StudentAdapter.java
+++ app/src/main/java/com/example/edgar/fryweek7/StudentAdapter.java
@@ -1,14 +1,26 @@
 package com.example.edgar.fryweek7;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by edgar on 3/03/17.
  */
 
 public class StudentAdapter extends BaseAdapter{
+
+    private ArrayList<Student> dataSource;
+    private Activity activity;
+
+    public StudentAdapter(ArrayList<Student> dataSource, Activity activity){
+        this.dataSource = dataSource;
+        this.activity = activity;
+    }
+
     @Override
     public int getCount() {
         return 0;
